17018ef8e22668574ffe5e2356a03f7fedba8374
weekndds <101921799+weekndds@users.noreply.github.com>
2024-12-28 17:59:18 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -45,7 +45,7 @@ local function GetGameInfo()
 end
 
 getgenv().Parvus = {
-    Source = "https://raw.githubusercontent.com/AlexR32/Parvus/" .. Branch .. "/",
+    Source = "https://raw.githubusercontent.com/weekndds/Parvus/" .. Branch .. "/",
 
     Games = {
         ["Universal" ] = { Name = "Universal",                  Script = "Universal"  },
